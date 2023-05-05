5ff8fd7a8b8ffce930a7d253d939fc11f9b9376f
fahmiihfaa <541221088@STUDENT.SMKTELKOM-PWT.SCH.ID>
2023-05-05 12:02:37 +0700

    Web Porto

---
+++ tugasdpk-k.js
@@ -0,0 +1,7 @@
+/* ======================================== typing animation ================================ */
+var typed = new Typed(".typing",{
+    Strings: [" ","Student", "SMK Telkom", "Purwokerto"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop:true
+})
